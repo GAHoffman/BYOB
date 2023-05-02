03681630dd97868c775cb280ab94dde26911011e
Garrett Hoffman <son0hoff@gmail.com>
2023-05-02 15:25:48 -0700

    updating public feed styling

--- ghi/src/Components/FriendRequest.jsx
+++ ghi/src/Components/FriendRequest.jsx
@@ -17,7 +17,7 @@ const FriendRequest = () => {
       </div>
 
       <div className="mt-4 flex justify-end gap-2">
-        <button className="bg-primary text-white py-2 px-5 rounded-lg">
+        <button className="bg-blue-500 text-white py-2 px-5 rounded-lg">
           Confirm
         </button>
         <button className="bg-gray-300 text-black py-2 px-5 rounded-lg">
--- ghi/src/Feed.js
+++ ghi/src/Feed.js
@@ -1,20 +1,21 @@
 import React from "react";
 import RightSidebar from "./Components/RightSidebar";
 import LeftSidebar from "./Components/LeftSidebar";
-// import PostForm from "./Posts/PostForm";
+import PostForm from "./Posts/PostForm";
+import Story from "./Components/Story";
 
 const Feed = () => {
   return (
     <>
       <LeftSidebar />
       <RightSidebar />
-      {/* <div className="mx-auto mt-4 max-w-[600px] 2xl:max-w-[800px] mb-10">
+      <div className="mx-auto mt-4 max-w-[600px] 2xl:max-w-[800px] mb-10">
         <Story />
-        <WhatsOnYourMind />
-        {posts.map((post) => {
+        <PostForm />
+        {/* {posts.map((post) => {
           return <Post key={post.id} id={post.id} data={post.data()} />;
-        })}
-        </div> */}
+        })} */}
+      </div>
     </>
   );
 };
